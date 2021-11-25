5256ffc7ecdf8472850da1bae5e777360cf52da2
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2021-11-25 19:53:13 +0100

    feat: intermal cli cmds

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: "[ -f './composer.json' ] && $(cat <<< $(jq  --arg v ${nextRelease.version} '.version=$v' ./composer.json ) > ./composer.json) || echo 'composer.json dont exists skipping' && [ -f './package.json' ] && $(cat <<< $(jq  --arg v ${nextRelease.version} '.version=$v' ./package.json ) > ./package.json) || echo 'package.json dont exists skipping'"
+        prepareCmd: "semantic-release-laravel bump ${nextRelease.version}"
       },
     ],
     [
---
+++ src/cli.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const argv = require("yargs");
+const chalk = require("chalk");
+const fs = require("fs");
+
+function fileExists(filepath) {
+  // check if file exists
+  if(fs.existsSync(filepath)) {
+    return true;
+  } else {
+    console.log(chalk.cyan(`${filepath} does not exist, skipping...`));
+  }
+}
+
+function writeToFile(filepath, data, version){
+  fs.writeFile(filepath, JSON.stringify(data, null, 4),
+      // callback function that is called after writing file is done
+      function(err) {
+          if (err) throw err;
+          console.log(chalk.green(`^${filepath} updated version -> ${version}`));
+  });
+}
+
+function bumpThatVesion(filepath, version){
+  try {
+    fs.readFile(filepath,
+        function(err, data) {
+            // json data
+            if (err) {
+              console.log(chalk.error(`Could not read data from file!`));
+              exit(1)
+            }
+            return writeToFile(filepath, JSON.parse(data), version)
+    });
+  } catch (e) {
+    console.log(chalk.error(`${e}`));
+  }
+
+}
+
+argv.command(
+  "bump",
+  "Bump version to composer.json, package.json",
+  (yargs) => {},
+  (argv) => {
+    if (fileExists('./composer.json')) {
+        bumpThatVesion('./composer.json', argv._[1])
+    }
+  }
+).argv;
